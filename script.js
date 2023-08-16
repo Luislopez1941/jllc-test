0cc19e12aa700d0ab670ec0197a04bd5b126ebbb
Jorge Luis <jorg3luislopezc1941@hotmail.com>
2023-08-15 23:53:49 -0500

    uno

--- script.js
+++ script.js
@@ -3,84 +3,65 @@ const button = document.querySelector('button');
 button.addEventListener('click', () => {
     button.classList.toggle('activo');
 });
-function setupSlider() {
-    const slider = document.querySelector(".slider");
-    const sliderSection = document.querySelectorAll(".slider__section");
-    const sliderSectionLast = sliderSection[sliderSection.length - 1];
 
-    slider.insertAdjacentElement('afterbegin', sliderSectionLast);
+let intervalId = null; // Variable para almacenar el ID del intervalo
+    let isSliderRunning = false; // Variable para controlar si el slider está en funcionamiento
 
-    let touchStartX = 0;
-    let touchEndX = 0;
-    const sensitivity = 50; // Sensibilidad del deslizamiento (ajústala según tus necesidades)
-    let autoSliderEnabled = true;
+    // Función para mostrar el slider
+    function startSlider() {
+      if (!isSliderRunning) {
+        isSliderRunning = true;
+        const slides = document.querySelectorAll(".slide");
+        let currentSlide = 0;
 
-    function Siguiente() {
-        const sliderSectionPrimero = document.querySelector(".slider__section");
-        slider.style.marginLeft = "-200%";
-        slider.style.transition = "all 0.5s";
-        setTimeout(function () {
-            slider.style.transition = "none";
-            slider.insertAdjacentElement('beforeend', sliderSectionPrimero);
-            slider.style.marginLeft = "-100%";
-        }, 500);
-    }
-
-    function Anterior() {
-        const sliderSectionUltimo = document.querySelector(".slider__section:last-child");
-        slider.style.marginLeft = "0";
-        slider.style.transition = "all 0.5s";
-        setTimeout(function () {
-            slider.style.transition = "none";
-            slider.insertAdjacentElement('afterbegin', sliderSectionUltimo);
-            slider.style.marginLeft = "-100%";
-        }, 500)
-    }
-
-    function handleTouchStart(event) {
-        touchStartX = event.touches[0].clientX;
-        autoSliderEnabled = false;
-    }
-
-    function handleTouchMove(event) {
-        touchEndX = event.touches[0].clientX;
-    }
-
-    function handleTouchEnd() {
-        const diffX = touchEndX - touchStartX;
-        if (diffX > sensitivity) {
-            Anterior();
-        } else if (diffX < -sensitivity) {
-            Siguiente();
+        function showSlide(slideIndex) {
+          slides.forEach((slide, index) => {
+            if (index === slideIndex) {
+              slide.classList.add("active");
+            } else {
+              slide.classList.remove("active");
+            }
+          });
         }
-        autoSliderEnabled = true;
+
+        function nextSlide() {
+          currentSlide = (currentSlide + 1) % slides.length;
+          showSlide(currentSlide);
+        }
+
+        showSlide(currentSlide); // Mostrar el primer contenedor al cargar la página
+        intervalId = setInterval(nextSlide, 3000); // Cambiar la imagen cada 3 segundos (ajusta según tus preferencias)
+      }
     }
 
-    // Agregar eventos de deslizamiento táctil
-    slider.addEventListener('touchstart', handleTouchStart);
-    slider.addEventListener('touchmove', handleTouchMove);
-    slider.addEventListener('touchend', handleTouchEnd);
-
-    // Detener el intervalo si ya estaba activo
-    if (interval) {
-        clearInterval(interval);
+    // Función para detener el slider
+    function stopSlider() {
+      const slides = document.querySelectorAll(".slide");
+      slides.forEach((slide) => {
+        slide.classList.remove("active");
+      });
+      if (intervalId !== null) {
+        clearInterval(intervalId); // Detener el intervalo si existe
+        intervalId = null; // Restablecer la variable intervalId
+      }
+      isSliderRunning = false;
     }
 
-    // Iniciar el intervalo para la función Siguiente si el auto slider está habilitado
-    if (autoSliderEnabled) {
-        interval = setInterval(Siguiente, 5000);
+    // Verificar el tamaño de la ventana al cargar la página y en cada cambio de tamaño
+    function checkWindowSize() {
+      if (window.innerWidth <= 768) {
+        startSlider();
+      } else {
+        stopSlider();
+      }
     }
-}
 
-// Llama a la función setupSlider cuando la página se haya cargado
-document.addEventListener("DOMContentLoaded", setupSlider);
+    // Verificar el tamaño de la ventana al cargar la página
+    checkWindowSize();
 
-// Referencia al intervalo actual
-let interval;
+    // Verificar el tamaño de la ventana cada vez que cambia su tamaño
+    window.addEventListener("resize", checkWindowSize);
 
-// Vuelve a activar el slider si el tamaño de la ventana cambia
-window.addEventListener("resize", function () {
-    // Reiniciar el slider al cambiar el tamaño de la ventana
-    clearInterval(interval);
-    setupSlider();
-});
+
+
+ 
